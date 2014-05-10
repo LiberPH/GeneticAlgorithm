6eddf7de4174977b12a59bb1a910b0590b714b02
Libertad Pantoja <lpantoja@lcg.unam.mx>
2014-05-09 20:49:12 -0500

    Update canberra.R

--- canberra.R
+++ canberra.R
@@ -2,20 +2,20 @@
 
 # ...
 
-argv <- commandArgs(T);
+argv <- commandArgs(T); #Passing arguments (Simulations' results)
 
 # error checking...
 
-x <- read.table(argv[1],sep = "");
+x <- read.table(argv[1],sep = ""); #Reading simulation result a)
 
 
 x <- x[,2];
 #x
-y <- read.table(argv[2],sep = "");
+y <- read.table(argv[2],sep = ""); #Reading simulation result b)
 y <- y[,2];
-diff <- dist(rbind(x, y), method = "canberra")  #Calculando distancia de canberra entre dos dinámicas
+diff <- dist(rbind(x, y), method = "canberra")  #Calculating Canberra's distance between two simulations' results
 sink("output.txt")
-cat(diff)
+cat(diff) #Send diff to bash
 sink()
 
 
